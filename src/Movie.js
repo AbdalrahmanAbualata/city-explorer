8818a41d4ea86b748150426a7625d37a04227395
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-08-17 20:48:13 +0300

    class03 don

--- src/Movie.js
+++ src/Movie.js
@@ -15,9 +15,7 @@ export class Movie extends React.Component {
                     <Card.Title style={{ textAlign:"center"}}><h2> 🎬{this.props.title} </h2></Card.Title>
                     <Card.Img style={{ marginLeft: "12.5%", width: "75%", height: "500px" }} variant="top" src={this.props.src} alt="aaaa" />
                     <Card.Body>
-                        <Card.Text>
-                            <p id="favorites"> 🍿overview:{this.props.overview} </p>
-                        </Card.Text>
+                        <p id="favorites"> 🍿overview:{this.props.overview} </p>
                         <h3> ⌚ released on: {this.props.date} </h3>
                        <h3 className="favorites"> 🗳 total votes:{this.props.vote} </h3>
                        <h3 id='dis'> 🎰 average votes :{this.props.avgVote} </h3>
